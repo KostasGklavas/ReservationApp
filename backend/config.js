60d37b674297afc9dbda91e4ae143daa0b92292f
KOSTAS GKLAVCAS <59598929+KostasGklavas@users.noreply.github.com>
2025-05-16 11:19:49 +0300

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  db: {
+    host: "localhost",
+    user: "YOUR_DB_USER",
+    password: "YOUR_DB_PASSWORD",
+    database: "reservation_app"
+  },
+  jwtSecret: "superSecretKey"
+};
